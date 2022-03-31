aebf261c7e07221e18df50620ab9146c0ad31db3
Quang Pham <102178224+springroll35@users.noreply.github.com>
2022-03-31 11:14:04 +0700

    Add files via upload

---
+++ DSA02002 - DAY SO 2.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define fi first
+#define sc second
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define endl "\n"
+
+const int maxn = 35;
+const ll mod = 1e9+7;
+typedef pair<int,int> ii;
+
+void process() {
+    int n;
+    cin >> n;
+    int dp[n][n];
+    for(int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        dp[n-1][i] = x;
+    }
+    for(int i = n-2; i >= 0; i--) {
+        for(int j = 0; j <= i; j++) {
+            dp[i][j] = dp[i+1][j] + dp[i+1][j+1];
+        }
+    }
+    for(int i = 0; i < n; i++) {
+        cout << '[';
+        for(int j = 0; j < i; j++) {
+            cout << dp[i][j] << ' ';
+        }
+        cout << dp[i][i] << "] ";
+    }
+    cout << endl;
+
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("inp.in", "r", stdin);
+        freopen("out.out", "w", stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int test;
+    cin >> test;
+    while(test--) {
+        process();
+    }
+    
+
+    return 0;
+}
---
+++ DSA02005 - HOAN VI XAU KY TU.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define fi first
+#define sc second
+#define pb push_back
+#define endl "\n"
+
+const int maxn = 1e5+1;
+const ll mod = 1e9+7;
+typedef pair<int,int> ii;
+
+int n;
+
+void process() {
+    string a;
+    cin >> a;
+    cout << a << ' ';
+    while (next_permutation(a.begin(),a.end())) {
+        cout << a << ' ';
+    }
+    cout << endl;
+}
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("inp.in", "r", stdin);
+        freopen("out.out", "w", stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int test;
+    cin >> test;
+    while(test--) {
+        process();
+    }
+    
+
+    return 0;
+}
